4eec8285b6fc9ae7930153288157c13246654791
Darshita <darshitachaurasia07@gmail.com>
2025-07-18 21:48:30 +0530

    timerdone

---
+++ Timer/welcome.js
@@ -0,0 +1,11 @@
+
+// Program to show a welcome message after 5 seconds
+// Function to display the welcome message
+function showWelcomeMessage() {
+    console.log("Welcome to our website!");
+}
+// Delay the welcome message by 5 seconds (5000 milliseconds)
+setTimeout(showWelcomeMessage, 5000);
+ // 5000ms = 5 seconds
+
+  
